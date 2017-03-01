343df98fa563ecdccae658b939d7b19c6778fe00
Frederik Hauke <frederik.hauke@gmx.net>
2017-03-01 19:22:49 +0000

    initial commit

---
+++ testStepper.c
@@ -0,0 +1,43 @@
+//Links: Schwarz-Rot, Rechts: Grün-Blau
+
+#include <wiringPi.h>
+int main (void)
+{
+	int dly = 2;
+	wiringPiSetup();
+	pinMode(6, OUTPUT);
+	pinMode(10, OUTPUT);
+	pinMode(11, OUTPUT);
+	pinMode(31, OUTPUT);
+	
+	
+	for(;;)
+	{
+		delay(dly);	
+		digitalWrite(6, HIGH) ; 
+		digitalWrite(10, LOW) ; 
+		digitalWrite(11, LOW) ; 
+		digitalWrite(31, LOW) ; 
+
+ 		delay(dly);     
+                digitalWrite(6, LOW) ; 
+                digitalWrite(10, HIGH) ; 
+                digitalWrite(11, LOW) ; 
+                digitalWrite(31, LOW) ; 
+		
+		delay(dly);     
+                digitalWrite(6, LOW) ; 
+                digitalWrite(10, LOW) ; 
+                digitalWrite(11, HIGH) ; 
+                digitalWrite(31, LOW) ; 
+ 		
+		delay(dly);     
+                digitalWrite(6, LOW) ; 
+                digitalWrite(10, LOW) ; 
+                digitalWrite(11, LOW) ; 
+                digitalWrite(31, HIGH) ; 
+
+
+	}
+	return 0;
+} 
